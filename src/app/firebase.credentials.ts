00c838458e7a90302e29732862c4bf3978a1623a
Nick Wichert <nick.wichert@icloud.com>
2018-01-24 22:08:28 -0600

    update to angularfire2@latest

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDw58E6b4HqHDotmHXcUo_9hgZrq-8Coz8",
+    authDomain: "ionicshoppinglist-7396f.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist-7396f.firebaseio.com",
+    projectId: "ionicshoppinglist-7396f",
+    storageBucket: "",
+    messagingSenderId: "485538119100"
+  };
