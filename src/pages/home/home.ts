bb3a51ec9e2bfa396fd8655820c7dea245a8cb33
Nick Wichert <nick.wichert@icloud.com>
2018-01-25 12:02:44 -0600

    completed C of CRUD

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../../models/item/item.model';
 
 @IonicPage()
 @Component({
@@ -8,8 +11,17 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
 
-  }
-
+  constructor(public navCtrl: NavController,private shopping: ShoppingListService) {
+      this.shoppingList$ = this.shopping
+        .getShoppingList() // this returns a db list
+        .snapshotChanges() // gets access to key and value pairs
+        .map(changes => { // mapping over each change to return a new object
+            return changes.map(c => ({
+              key: c.payload.key,
+              ...c.payload.val()
+            }));
+          });
+    }
 }
